f91a85a474555a172ca276749cd5f02f6f7cb331
MaratXcode <kosjachok8@yandex.ru>
2024-08-13 13:56:51 +0300

    Initial Commit

---
+++ ListAppSwiftUI/ListAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppSwiftUIApp.swift
+//  ListAppSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ListAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
